89290ff5e6a4063434b39ab58301a903a26c4fad
Alex Bakun <bakun.alex93@gmail.com>
2023-02-09 13:24:43 +0200

    fix Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button {...props} className={'botton ' + props.className}>Button</button>
+    <button {...props} className={'botton ' + props.className}></button>
   )
 }
 
