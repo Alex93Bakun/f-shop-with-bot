7b766dc13afc0d3a6b6e28fd2f59690c242ef1bd
Alex Bakun <bakun.alex93@gmail.com>
2023-02-09 15:32:27 +0200

    change Form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { useTelegram } from '../hooks/useTelegram';
 
 import './Form.css';
 
 const Form = () => {
-    return <div>Form</div>;
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const { tg } = useTelegram()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Надіслати дані'
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!city || !country) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }, [country, city]);
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value);
+    };
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value);
+    };
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value);
+    };
+
+    return (
+        <div className="form">
+            <h3>Введіть ваші дані</h3>
+            <input
+                className="input"
+                type="text"
+                placeholder="Країна"
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className="input"
+                type="text"
+                placeholder="Місто"
+                value={city}
+                onChange={onChangeCity}
+            />
+            <select className="select" value={subject} onChange={onChangeSubject}>
+                <option value="physical">Фіз. особа</option>
+                <option value="legal">Юр. особа</option>
+            </select>
+        </div>
+    );
 };
 
 export default Form;
